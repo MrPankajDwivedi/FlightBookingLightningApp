53951771b4582b638e3286743bb674fa6c7cb0a5
MrPankajDwivedi <developer.dbrothers@gmail.comcd>
2017-10-04 21:55:16 +0530

    Measure chanages to UI

--- src/aura/AddPassengerDetails/AddPassengerDetailsController.js
+++ src/aura/AddPassengerDetails/AddPassengerDetailsController.js
@@ -24,15 +24,26 @@
      $A.enqueueAction(action);    
 	},
 	goToEdit : function(component, event, helper) {
-	document.getElementById('EditPassenger').scrollIntoView();
+	document.getElementById('Passenger1').scrollIntoView();
       var index=event.getSource().get("v.value");
-        console.log("Value clicked:"+index);  
-        var passengers=component.get("v.passengers");   
-        console.log(passengers[index].Name__c);
-       component.set("v.passenger",passengers[index]);
-       
+        console.log("Value cliced:"+index);  
+        if(index==0){
+            document.getElementById('Passenger1').style.display = 'block'; 
+            var passengers=component.get("v.passengers");   
+            console.log(passengers[index].Name__c);
+            component.set("v.passenger",passengers[index]);
+        }else{
+            document.getElementById('Co_Passenger').scrollIntoView();
+            var passengers=component.get("v.passengers");   
+            console.log(passengers[index].Name__c);
+            component.set("v.co_passenger",passengers[index]);
+            var a = component.get('c.update_Co_Passenger');
+            $A.enqueueAction(a);
+            
+        }  
 	},
     AddPassenger: function(component) {
+        document.getElementById('PassengerDeatils').scrollIntoView();
         var num=component.get("v.number_of_passenger");
         var p=component.get("v.passenger");
         var customer={'sobjectType':'CustomerDetail__c',
@@ -40,41 +51,91 @@
                       'Email__c':p.Email__c,
                       'DOB__c':p.DOB__c,
                       'Age__c':p.Age__c,
-                      'Mobile_No__c':p.Mobile_No__c
+                      'Mobile_No__c':p.Mobile_No__c,
+                      'Gender__c':component.find("gender__ddl").get("v.value")
                      }
         console.log("Customer:"+customer.Name__c);
         
-     
-       // var passenger=component.get("v.passenger"); 
+        
+        // var passenger=component.get("v.passenger"); 
         var passengers=component.get("v.passengers"); 
-      console.log(passengers);
-        passengers.push(customer);
-  console.log(passengers);         
-       component.set("v.passengers",passengers);
+        console.log(passengers);
+        passengers[0]=p;
+        console.log(passengers);         
+        component.set("v.passengers",passengers);
         num=num+1;
         console.log("Number:"+num);
+        component.set("v.number_of_passenger",num);
+        document.getElementById('Passenger1').style.display = 'none';
+    },
+    Add_Co_Passenger:function(component) {
+          document.getElementById('PassengerDeatils').scrollIntoView();
+        var num=component.get("v.number_of_passenger");
+        var p=component.get("v.co_passenger");
+        var customer={'sobjectType':'CustomerDetail__c',
+                      'Name__c':p.Name__c,
+                      'Age__c':p.Age__c,
+                      'Gender__c':component.find("gender__ddl").get("v.value")
+                     }
+        console.log("Customer:"+customer.Name__c);
         
+        
+        // var passenger=component.get("v.passenger"); 
+        var passengers=component.get("v.passengers"); 
+        console.log(passengers);
+        passengers.push(customer);
+        console.log(passengers);         
+        component.set("v.passengers",passengers);
+        num=num+1;
+        console.log("Number:"+num);
         component.set("v.number_of_passenger",num);
         
-	},
+        
+        
+    },
+      update_Co_Passenger:function(component,index) {
+          document.getElementById('PassengerDeatils').scrollIntoView();
+        var num=component.get("v.number_of_passenger");
+        var p=component.get("v.co_passenger");
+        var customer={'sobjectType':'CustomerDetail__c',
+                      'Name__c':p.Name__c,
+                      'Age__c':p.Age__c,
+                      'Gender__c':component.find("gender__ddl").get("v.value")
+                     }
+        console.log("Customer:"+customer.Name__c);
+        
+        
+        // var passenger=component.get("v.passenger"); 
+        var passengers=component.get("v.passengers"); 
+        console.log(passengers);
+        passengers[index]=customer;
+        console.log(passengers);         
+        component.set("v.passengers",passengers);
+        num=num+1;
+        console.log("Number:"+num);
+        component.set("v.number_of_passenger",num);
+        
+        
+        
+    },
     handleAppData:function(component, event, helper){
         
-       var message = event.getParam("message");
-
-        // set the handler attributes based on event data
-       console.log(message);
-       // var params=event.getParam("flight");
-      //  var flight=params.flight;
-        //console.log(flight);
-      //  component.set("v.flight",params);
+        var message = event.getParam("message");
         
-	},
+        // set the handler attributes based on event data
+        console.log(message);
+        // var params=event.getParam("flight");
+        //  var flight=params.flight;
+        //console.log(flight);
+        //  component.set("v.flight",params);
+        
+    },
     handleApplicationEvent : function (component, event) {
         var message = event.getParam("message");
-		var flight=event.getParam("flight");
+        var flight=event.getParam("flight");
         component.set("v.flight",flight);
         // set the handler attributes based on event data
-       console.log(message+flight.Name);
+        console.log(message+flight.Name);
     },
     goToPayment: function (component, event) {
           var cmpEvent=component.getEvent("bubblingEvent");
--- src/aura/PaymentPage/PaymentPageController.js
+++ src/aura/PaymentPage/PaymentPageController.js
@@ -7,5 +7,31 @@
          component.set("v.passengers",passengers);
         // set the handler attributes based on event data
        console.log("In payment page");
-    }
+    },
+    bookFlightClient:function(component, event, helper) {
+          var p=component.get("v.passengers[0]");
+        console.log(p.Id+":"+component.get("v.flight.Id"));
+    
+        var action = component.get("c.bookFlight");
+        
+       action.setParams({
+            customer:p,
+            flight:component.get("v.flight"),
+            journeyDate:component.get("v.flight.Journey_Date__c"),
+            co_passenger:'Rahul,46,Male;Suman,23,Female'
+        });
+       
+        action.setCallback(this, function(response){
+            var name = response.getState();
+            if (name === "SUCCESS") {
+                console.log("Succesfully booked Booking Id:"+response.getReturnValue().Id);
+                window.print();
+                
+            }else if (response.getState() === "ERROR") {
+                $A.log("Errors", response.getError());
+            }
+        });
+    
+     $A.enqueueAction(action);    
+	}
 })
